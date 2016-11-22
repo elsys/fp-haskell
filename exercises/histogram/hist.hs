0615b8dcd9ce53a65e023fe43105d8c8a116518a
Georgi Nakov <nakov.gl@gmail.com>
2016-11-22 22:07:19 +0200

    Solution for histogram

--- exercises/histogram/hist.hs
+++ exercises/histogram/hist.hs
@@ -1,28 +1,28 @@
-import Data.List(intersperse)
+module Histogram where
 
-base = [0..9]::[Int]
-base_string = ['0'..'9'] ++ "\n"
-symbol = '*'
+baseString::String
+baseString = ['0'..'9']
 
 count :: (Eq a) => a -> [a] -> Int
-count = undefined
+count item list = length [x | x <- list, x == item]
 
 frequency :: [Int] -> [Int]
-frequency = undefined
+frequency list = [count x list | x <- [0..9]]
 
 decrement :: [Int] -> [Int]
-decrement = undefined
+decrement list = [x - 1 | x <- list]
 
 hasPositive :: [Int] -> Bool
-hasPositive = undefined
+hasPositive list = not (null [x | x <- list, x > 0])
 
 makeLine :: [Int] -> String
-makeLine = undefined
-
+makeLine list = [if x > 0 then '*' else ' ' | x <- list]
 
 freq2hist :: [Int] -> [String]
-freq2hist = undefined
+freq2hist x
+    | hasPositive x = makeLine x : freq2hist (decrement x)
+    | otherwise     = []
 
 histogram :: [Int] -> String
-histogram = undefined
-
+histogram x = unlines (reverse histLines ++ [baseString])
+    where histLines = freq2hist (frequency x)
